e60f057bff3adfe651fb5e309fdc0573856edb81
zeyger <zeyger@icloud.com>
2019-03-31 13:39:19 +0000

    Transform image to ready for test launches

--- transform_image.sh
+++ transform_image.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-th neural_style.lua -gpu -1 -print_iter 1 -image_size 16
+th neural_style.lua -style_image input/style.jpg -content_image input/image.jpg -output_image output/output.png -gpu -1 -print_iter 1 -num_iterations 101 -image_size 16
+
