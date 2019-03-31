72fd7dfedf6612e6e6d95e872437047b41254630
zeyger <zeyger@icloud.com>
2019-03-31 13:49:00 +0000

    Handler MVP

--- handler.py
+++ handler.py
@@ -1,10 +1,13 @@
 from subprocess import Popen, PIPE, STDOUT
 
-#cmd = 'echo "rock and roll"'
+
 cmd = 'sh transform_image.sh' 
 p = Popen(cmd, stdout = PIPE, 
         stderr = STDOUT, bufsize=1, shell = True)
 for line in iter(p.stdout.readline, b''):
     line = line.decode("utf-8")
-    #print(line.replace('r', ' kek'))
     print(line)
+    if "reached max number of iterations" in line:
+        Popen(['python3', 'transmitter.py'])
+
+
